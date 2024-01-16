c6a13856125fa8a6c9d061de2489f75d12f143b5
Benjahh <lucasbenja09@hotmail.com>
2024-01-15 22:17:17 -0300

    feat: navbar component and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
